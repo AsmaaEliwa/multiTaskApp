d1269dedcb24ac8212ae808cb645cc628fd6529f
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2023-12-15 06:09:08 -0500

    setting up app

---
+++ multiTaskApp/multiTaskAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  multiTaskAppApp.swift
+//  multiTaskApp
+//
+//  Created by asmaa gamal  on 15/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct multiTaskAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
